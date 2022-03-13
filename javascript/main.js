dd8662357ebe43b0bca90fa03c991f5c3cd2ffb0
Alessio <alessio26manfredini@gmail.com>
2022-03-13 18:02:06 +0100

    Basic structure and logic
    I added the basic structure of the project in html
    whit a basic style to see the change in the DOM.
    
    I implemented the logic to color the squares and
    to change color based on a input.

--- javascript/main.js
+++ javascript/main.js
@@ -1,31 +1,110 @@
-const rangeValue = document.getElementById("range-selector");
+const rangeSelector = document.getElementById("range-selector");
 const showRangeValue = document.getElementById("show-range-value");
-const canvas = document.querySelector(".canvas"); 
-showRangeValue.textContent = rangeValue.value + " x " + rangeValue.value;
+const canvas = document.querySelector(".canvas");
+const colorSelector = document.getElementById("color-selector");
+let mousePressed = false;
 
 
-rangeValue.addEventListener("mousemove", () => {
-    showRangeValue.textContent = rangeValue.value + " x " + rangeValue.value;
-    createDivCanvas(rangeValue.value);
-})
+// display initial value
+showRangeValue.textContent = rangeSelector.value + " x " + rangeSelector.value;
+createDivCanvas(rangeSelector.value);
 
-function createDivCanvas(number) {
-    for(let i = 0; i < number * number; i++){
-        let div = document.createElement("div");
-        div.setAttribute("class", "grid-element");
 
-        canvas.style.cssText=`width: 45%;
-            background-color: #fff;
-            height: 34em;
-            width: 34em;
-            display: grid;
-            grid-template-columns: repeat(${number}, 1fr);
-            grid-template-rows: repeat(${number}, 1fr);
-        `;
+//function
+    // create canvas resolution
+    function createDivCanvas(number) {
+        for(let i = 0; i < number * number; i++){
+            let div = document.createElement("div");
+            div.setAttribute("class", "grid-element");
 
-        document.querySelector(".canvas").prepend(div);
+            canvas.style.cssText=`width: 45%;
+                background-color: #fff;
+                height: 34em;
+                width: 34em;
+                display: grid;
+                grid-template-columns: repeat(${number}, 1fr);
+                grid-template-rows: repeat(${number}, 1fr);
+            `;
+
+            document.querySelector(".canvas").prepend(div);
+        }
     }
-}
 
-createDivCanvas(16)
+
+    // update color value and cancel color class
+    function updateColorValue() {
+        document.querySelectorAll(".grid-element").forEach((element) => {
+            element.classList.remove("color1");
+            element.classList.remove("color2");
+            element.classList.remove("color3");
+        })
+    }
+
+
+    // set color attribute on target
+    function colorDiv(events) {
+        if(mousePressed === true){
+            if(events.target.classList.contains("color1")){
+                if(events.target.classList.contains("color2")){
+                    events.target.classList.add("color3");
+                    events.target.style.cssText = `
+                        background-color: ${colorSelector.value};
+                        opacity: 1;
+                    `;
+                    return
+                }
+                events.target.classList.add("color2");
+                events.target.style.cssText = `
+                        background-color: ${colorSelector.value};
+                        opacity: 0.66;
+                `;
+            }else{
+                events.target.classList.add("color1");
+                events.target.style.cssText = 
+                    `
+                    background-color: ${colorSelector.value};
+                    opacity: 0.33;
+                `;
+            }
+        }
+    }
+
+
+
+
+
+// events
+    // display resolution
+    rangeSelector.addEventListener("mousemove", () => {
+        showRangeValue.textContent = rangeSelector.value + " x " + rangeSelector.value;
+    })
+
+    // change canvas resolution
+    rangeSelector.onchange = () => {createDivCanvas(rangeSelector.value)};
+
+    // activate drowing while mouse is pressed
+    document.addEventListener("mousedown", (events) => {
+        if(events.target === rangeSelector){
+            
+        }else{
+            mousePressed = true;
+            console.log("mouse is pressed");
+            events.preventDefault();
+        }
+    })
+
+    // deactivate drowing when mouse is realised
+    document.addEventListener("mouseup", () => {
+        mousePressed = false;
+        console.log("mouse is realised");
+    })
+
+    // color div target
+    canvas.addEventListener("mouseover", colorDiv);
+
+    // change color for drawing and reset class
+    colorSelector.onchange = () => {updateColorValue()};
+
+
+
 
